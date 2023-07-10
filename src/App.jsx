11b7b79ba57f23f08b985bceae1a5baa532634dd
Sandro <vipsandro11@gmail.com>
2023-07-10 16:58:13 +0400

    optimize bundle size

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,34 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import AppLayout from "./pages/AppLayout";
-import Home from "./pages/Home";
-import Pricing from "./pages/Pricing";
-import Product from "./pages/Product";
+import { lazy, Suspense } from "react";
 import CityList from "./components/CityList";
 import SingleCity from "./components/SingleCity";
 import CountryList from "./components/CountryList";
 import Form from "./components/Form";
+const Home = lazy(() => import("./pages/Home"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Product = lazy(() => import("./pages/Product"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const Loading = lazy(() => import("./components/Loading"));
+
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="product" element={<Product />} />
-        <Route path="app" element={<AppLayout />}>
-          <Route index element={<Navigate replace to="cities" />} />
-          <Route path="cities" element={<CityList />} />
-          <Route path="cities/:id" element={<SingleCity />} />
-          <Route path="countries" element={<CountryList />} />
-          <Route path="form" element={<Form />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="product" element={<Product />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<Navigate replace to="cities" />} />
+            <Route path="cities" element={<CityList />} />
+            <Route path="cities/:id" element={<SingleCity />} />
+            <Route path="countries" element={<CountryList />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
